5e0197b887812bb628a8abe0b84f47768f8d43ae
zzxwill <zzxwill@gmail.com>
2020-11-25 16:50:24 +0800

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,67 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/login',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+    ],
+  },
+  {
+    path: '/application',
+    name: 'application',
+    icon: 'bars',
+    component: './Application',
+    routes: [
+      {
+        path: '/application/demo',
+        name: 'demo',
+        icon: 'appstore',
+        component: './Demo',
+      },
+    ],
+  },
+  // {
+  //   path: '/demo',
+  //   name: 'demo',
+  //   icon: 'appstore',
+  //   component: './Demo',
+  // },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './ListTableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
