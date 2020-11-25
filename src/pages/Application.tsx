dc74f02c5acccfbdfb71f5fdb61fbb2a359155ac
zzxwill <zzxwill@gmail.com>
2020-11-25 17:30:19 +0800

    add form

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -2,9 +2,96 @@ import React from 'react';
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { Card, Typography, Alert } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
+import { Form, Input, Button,Select } from 'antd';
+import ReactDOM from 'react-dom';
+
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+const Demo = () => {
+  const [form] = Form.useForm();
+
+  const onGenderChange = (value: any) => {
+    switch (value) {
+      case 'male':
+        form.setFieldsValue({ note: 'Hi, man!' });
+        return;
+      case 'female':
+        form.setFieldsValue({ note: 'Hi, lady!' });
+        return;
+      case 'other':
+        form.setFieldsValue({ note: 'Hi there!' });
+        return;
+    }
+  };
+
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male',
+    });
+  };
+
+  return (
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item name="note" label="Note" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+        <Select
+          placeholder="Select a option and change input text above"
+          onChange={onGenderChange}
+          allowClear
+        >
+          <Option value="male">male</Option>
+          <Option value="female">female</Option>
+          <Option value="other">other</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        noStyle
+        shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+      >
+        {({ getFieldValue }) => {
+          return getFieldValue('gender') === 'other' ? (
+            <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+          ) : null;
+        }}
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+        <Button type="link" htmlType="button" onClick={onFill}>
+          Fill form
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default (): React.ReactNode => (
-  <PageContainer content=" 这个页面只有 admin 权限才能查看">
+  <PageContainer content=" Application Management">
     <Card>
       <Alert
         message="umi ui 现已发布，欢迎使用 npm run ui 启动体验。"
@@ -27,5 +114,10 @@ export default (): React.ReactNode => (
       </a>
       。
     </p>
+    <Button type="primary">Primary Button</Button>
+    <div id="Demo"/>
+
   </PageContainer>
 );
+
+ReactDOM.render(<Demo />, document.getElementById("Demo"));
