ca001750dfd3bf18398c79d6e0167f5e77f60cf0
zzxwill <zzxwill@gmail.com>
2021-02-05 15:45:27 +0800

    show form data after button clicked

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -6,7 +6,8 @@ import FormRender from 'form-render/lib/antd';
 // import '@alifd/next/dist/next.min.css';
 import { Button } from 'antd';
 
-const schema = {
+let schema = {};
+const schema2 = {
   properties: {
     cmd: {
       description: 'Commands to run in the container',
@@ -84,84 +85,6 @@ const schema = {
   required: ['image', 'port'],
   type: 'object',
 };
-const schemaRight = {
-  type: 'object',
-  required: ['image', 'port'],
-  properties: {
-    cmd: {
-      title: 'cmd',
-      description: 'Commands to run in the container',
-      type: 'array',
-      items: {
-        type: 'string',
-      },
-    },
-    env: {
-      title: 'env',
-      description: 'Define arguments by using environment variables',
-      type: 'array',
-      items: {
-        type: 'object',
-        required: ['name'],
-        properties: {
-          name: {
-            title: 'name',
-            description: 'Environment variable name',
-            type: 'string',
-          },
-          value: {
-            title: 'value',
-            description: 'The value of the environment variable',
-            type: 'string',
-          },
-          valueFrom: {
-            title: 'valueFrom',
-            description: 'Specifies a source the value of this var should come from',
-            type: 'object',
-            required: ['secretKeyRef'],
-            properties: {
-              secretKeyRef: {
-                title: 'secretKeyRef',
-                description: "Selects a key of a secret in the pod's namespace",
-                type: 'object',
-                required: ['name', 'key'],
-                properties: {
-                  name: {
-                    title: 'properties',
-                    description: "The name of the secret in the pod's namespace to select from",
-                    type: 'string',
-                  },
-                  key: {
-                    title: 'key',
-                    description: 'The key of the secret to select from. Must be a valid secret key',
-                    type: 'string',
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    image: {
-      title: 'image',
-      description: 'Which image would you like to use for your service\n+short=i',
-      type: 'string',
-    },
-    port: {
-      title: 'port',
-      description: 'Which port do you want customer traffic sent to\n+short=p',
-      type: 'integer',
-      default: 80,
-    },
-    cpu: {
-      title: 'cpu',
-      description:
-        'Number of CPU units for the service, like `0.5` (0.5 CPU core), `1` (1 CPU core)',
-      type: 'string',
-    },
-  },
-};
 
 function Demo() {
   const [formData, setData] = useState({});
@@ -176,8 +99,13 @@ function Demo() {
     }
   };
 
+  function onMouseClick() {
+    setData(schema2);
+  }
+
   return (
     <div style={{ maxWidth: 600 }}>
+      <button onClick={onMouseClick}>Show Form Data</button>
       <FormRender
         schema={schema}
         formData={formData}
